199e0056b983f99a09cd3174653503b4f1e716db
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-11-23 11:17:19 -0300

    funcionalidades

--- src/contacorrente/ContaCorrente.java
+++ src/contacorrente/ContaCorrente.java
@@ -11,13 +11,13 @@ public class ContaCorrente {
         String execucao,nome;
         int nconta,numero,escolha;
         Movimentacao c=null;
-        double saldoinicial,valor;
+        double saldoinicial, saldoinicialc, valor, valorc = 0;
        
         while(continuar==true){
 //Menu inicial da aplicação
             System.out.println("Digite 1 para criar conta, "
                     + "2 para sacar, 3 para depositar, 4 para ver"
-                    + " o saldo e 5 para status geral da conta: ");
+                    + " o saldo e 5 para ver extrato geral da conta: ");
             escolha = in.nextInt();
 //criando uma nova conta no banco
             if(escolha==1){           
@@ -27,20 +27,36 @@ public class ContaCorrente {
                 numero = in.nextInt();
                 System.out.println("Digite o depósito inicial: ");
                 saldoinicial = in.nextDouble();
-                c = new Movimentacao(numero,nome,saldoinicial);
+                System.out.println("Digite o valor que deseja para o crédito:");
+                saldoinicialc = in.nextDouble();
+                c = new Movimentacao(numero,nome,saldoinicial,saldoinicialc);
                 System.out.println("Saldo atual: "+c.getSaldo());
+                System.out.println("Saldo atual de crédito: "+c.getCredito());
+                System.out.println(" ");
             }
 //definindo um valor para sacar
             if(escolha==2){
                 System.out.println("Digite o valor que deseja sacar: ");
                 valor = in.nextDouble();
-                boolean saque_efetuado = c.sacar(valor);
-                if (saque_efetuado==true){
-                    System.out.println("Saque realizado com sucesso!");
+                /*System.out.println("Confirme o valor que deseja sacar:  ");
+                valorc = in.nextDouble();*/
+                double saque_efetuado = c.sacar(valor, valorc);
+                if (saque_efetuado==1){
+                    System.out.println("Transferencia realizada com sucesso!");
                     System.out.println("Saldo atual: "+c.getSaldo());
-                }else{
-                    System.out.println("Valor inválido!");
+                    System.out.println(" ");
+                    
                 }
+                if(saque_efetuado==0){
+                    System.out.println("saldo removido do crédito! ");
+                    System.out.println(" ");
+                    System.out.println("Saldo atual de crédito: "+c.getCredito());
+                    System.out.println(" ");
+                }
+                /*boolean tirar_debito = c.sacarc(valor);
+                if (tirar_debito==true){  
+                    System.out.println("Saldo atual em crédito: "+c.getCredito());
+                }*/
             }
 //definindo um valor para depositar
             if(escolha==3){
@@ -49,29 +65,39 @@ public class ContaCorrente {
                 boolean deposito_efetuado = c.setSaldo(valor);
                 if(deposito_efetuado==true){
                     System.out.println("Depósito realizado com sucesso!");
-                    System.out.println("Saldo atual: "+c.getSaldo());}
+                    System.out.println("Saldo atual: "+c.getSaldo());
+                    System.out.println(" ");
+                }
                else{
                 System.out.println("Valor inválido.");
+                System.out.println(" ");
                 }
             }
 //mostrando apenas o saldo
             if(escolha==4){
                 System.out.println("Saldo atual: "+c.getSaldo());
+                System.out.println(" ");
             }
 //mostrando todas as informações da conta
             if(escolha==5){
                 System.out.println("Titular da conta: "+c.getTitular());
                 System.out.println("Número da conta: "+c.getNumero());
                 System.out.println("Saldo atual: "+c.getSaldo());
-                System.out.println("Saques realizados hoje: " + c.saques + "\n");
+                System.out.println("Saldo atual em crédito: "+c.getCredito());
+                System.out.println("Saques realizados hoje: " + c.saques);
+                System.out.println("Depositos realizados hoje:" + c.depositos + "\n");
+                System.out.println(" ");
             }
 //perguntando se deseja finalizar ou continuar na aplicação
-            System.out.println("Deseja continuar? s - sim/ n - não");
+            System.out.println(" ");
+            System.out.println ("Deseja continuar? s - sim/ n - não");
+            System.out.println(" ");
             execucao = in.next();
             if(execucao.equals("n")){
+                System.out.println(" ");
+                System.out.println("Operações finalizadas pelo usuário");
                 continuar=false;
             }
-            
-        }
+    }
     }
 }
--- src/contacorrente/Movimentacao.java
+++ src/contacorrente/Movimentacao.java
@@ -1,20 +1,46 @@
 package contacorrente;
 
 public class Movimentacao {
-    int numero, saques;
+    int numero, saques, depositos;
     String titular;
     double saldo;
+    double saldoc;
    
-    public Movimentacao(int numero,String nome,double saldo){
+    public Movimentacao(int numero,String nome,double saldo,double saldoc){
         this.numero=numero;
         this.titular=nome;
         this.saldo=saldo;
+        this.saldoc=saldoc;
         saques=0;
     }
-    boolean sacar(double valor){
-        if(valor>0){
+    /*boolean sacarc (double valorc){
+        if(valorc<=saldoc){
+            this.saldoc-=valorc;
+            System.out.println("Saldo removido do crédito disponivel");
+            return true;
+        }else{
+            System.out.println("Não tem crédito");
+            return false;
+        }
+    }*/
+     double sacar(double valor, double valorc){
+        if(valor<=saldo){
             this.saldo-=valor;
             saques++;
+            return 1;
+        }else{
+            this.saldoc-=valor;
+            saques++;
+            return 0;
+        }
+    }
+
+    double getCredito(){
+        return saldoc;
+    }
+    boolean setCredito(double valorc){
+        if(valorc>0){
+            this.saldoc+=valorc;
             return true;
         }else{
             return false;
@@ -26,6 +52,7 @@ public class Movimentacao {
     boolean setSaldo(double valor){
         if(valor>0){
             this.saldo+=valor;
+            depositos++;
             return true;
         }else{
             return false;
